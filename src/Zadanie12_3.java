bb0848edac3de2fcbeedc88e38d51b8cf7ae1dbc
Piotrek <p.cz@outlook.com>
2018-08-12 17:56:06 +0200

    Zadanie 12.3 + plik

---
+++ src/Zadanie12_3.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Zadanie12_3 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        int duplicate = 0;
+
+        File file = new File("Plik.csv");
+        Scanner read = new Scanner(file);
+
+        ArrayList<Integer> liczby = new ArrayList<>();
+        TreeSet<Integer> set = new TreeSet<>();
+        Map<Integer, Integer> repeats = new HashMap<>();
+
+        while (read.hasNextInt()) {
+            liczby.add(read.nextInt());
+        }
+
+        liczby.sort(Integer::compareTo);
+
+        for (int i = 0; i < liczby.size() - 1; i++) {
+            if (liczby.get(i).equals(liczby.get(i + 1))) {
+                set.add(liczby.get(i));
+                repeats.put(liczby.get(i), ++duplicate + 1);
+            } else {
+                duplicate = 0;
+            }
+        }
+        for (int numerySort : liczby) {
+            System.out.print(numerySort + " ");
+        }
+        System.out.println("\n");
+
+        for (int sety : set) {
+            System.out.println(sety + " - liczba wystąpień: " + repeats.get(sety));
+        }
+        read.close();
+    }
+}
